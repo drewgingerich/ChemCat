a77e18a5daab581b78b75a52c46d10f8dae509f1
Drew Gingerich <gingerich93@gmail.com>
2018-06-18 18:01:02 -0700

    Improve event display.

--- Assets/Scripts/Event/EventDisplay.cs
+++ Assets/Scripts/Event/EventDisplay.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class EventDisplay : MonoBehaviour {
 
 	[SerializeField] EventDriver driver;
-	[SerializeField] Text placeholderText;
+	[SerializeField] GameObject placeholderText;
+	[SerializeField] GameObject countdownDivider;
+	[SerializeField] GameObject countdownLabelText;
 	[SerializeField] Text titleText;
 	[SerializeField] Text effectText;
 	[SerializeField] Text flavorText;
@@ -20,10 +22,12 @@ public class EventDisplay : MonoBehaviour {
 	}
 
 	void ClearDisplay() {
-		placeholderText.gameObject.SetActive(true);
+		placeholderText.SetActive(true);
 		titleText.text = "";
 		effectText.text = "";
 		flavorText.text = "";
+		countdownDivider.SetActive(false);
+		countdownLabelText.SetActive(false);
 		countdownText.text = "";
 	}
 
@@ -32,6 +36,8 @@ public class EventDisplay : MonoBehaviour {
 		titleText.text = driver.activeEvent.title;
 		effectText.text = driver.activeEvent.effect;
 		flavorText.text = driver.activeEvent.flavor;
+		countdownDivider.SetActive(true);
+		countdownLabelText.SetActive(true);
 		countdownText.text = Mathf.FloorToInt(driver.presentationTime - driver.timer).ToString();
 	}
 }
